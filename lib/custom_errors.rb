05da0ad43a0ef5d028424602aeb31181e88e79ac
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-03 23:46:36 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,13 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != person
+      raise PartnerError
+    else
     person.partner = self
   end
-
+  class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
